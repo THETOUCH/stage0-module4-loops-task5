35360c863cc1b7e5adefa44039ed5b6fdb561ebc
THETOUCH <super.ilgiznex@gmail.com>
2023-10-20 20:20:27 +0600

    123

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,32 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-
+        if (sideLength <= 0) {
+            System.out.print("");
+        } else {
+            int sum = 0;
+            int sred = 0;
+            int kolvo = 0;
+            for (int i = 1; i <= sideLength; i++) {
+                sum += i;
+                kolvo += 1;
+            }
+            sred = sum / kolvo;
+            for (int row = 0; row < sideLength; row++) {
+                for (int col = 0; col < sideLength; col++) {
+                    if (row == sred - 1) {
+                        System.out.print("8");
+                    } else {
+                        if (col == sred - 1) {
+                            System.out.print("8");
+                        }
+                        if (col != sideLength-1) {
+                            System.out.print(" ");
+                        } else break;
+                    }
+                }
+                System.out.print("\n");
+            }
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -2,6 +2,20 @@ package school.mjc.stage0.loops.task5;
 
 public class Square {
     public void printSquareFrom8s(int sideLength){
-
+        for (int row = 0; row < sideLength; row++) {
+            for (int col = 0; col < sideLength; col++) {
+                if (row == 0 || row == sideLength-1) {
+                    System.out.print("8");
+                } else {
+                    System.out.print("8");
+                    for (int i = 0; i < sideLength-2; i++) {
+                        System.out.print(" ");
+                    }
+                    System.out.print("8");
+                    break;
+                }
+            }
+            System.out.print("\n");
+        }
     }
 }
